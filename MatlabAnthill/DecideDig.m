4a85955441133ad62e789335e5d644a55ee359c1
Emil <emiljanQ3@hotmail.com>
2020-12-02 16:52:29 +0100

    Implemented Decide dig partially

---
+++ MatlabAnthill/DecideDig.m
@@ -0,0 +1,19 @@
+function [willDig, digPos] = DecideDig(ant, world, C)
+    [perceptionXs, perceptionYs] = AntPerceptionArea(ant);
+    [bodyXs, bodyYs] = AntBodyArea(ant);
+    
+     isInDepression = sum(world.sand(perceptionXs, perceptionYs)) > 11;
+     
+     Q = mean(world.digPheromone(bodyXs, bodyYs)); %Mean pheromone level
+     
+     if isInDepression
+         willDig = rand < C.f_sa;
+         willDig = willDig || rand < C.f_pa(Q);
+     else
+         willDig = rand < C.f_sa;
+         willDig = willDig || rand < C.f_pa(Q);
+     end
+     
+     %TODO how to decide dig position?
+     
+end
