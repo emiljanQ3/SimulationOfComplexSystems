540edcf8eba7efad3e39c994be955b17c73a8199
Johan Nilsson <johan.laberg@outlook.com>
2020-12-03 15:31:25 +0100

    Ants are now palces inside world

--- MatlabAnthill/AntBodyArea.m
+++ MatlabAnthill/AntBodyArea.m
@@ -1,22 +1,21 @@
 function [xs,ys] = AntBodyArea(ant)
-    [verticalBodyX, vertivalBodyY] = meshgrid(-1:1, -2:2);
+    [verticalBodyX, verticalBodyY] = meshgrid(-1:1, -2:2);
     diagonalBodyX = [1,2,0,1,2,-1,0,1,-2,-1,0,-2,-1];
     diagonalBodyY = [2,2,1,1,1,0,0,0,-1,-1,-1,-2,-2];
     
     if ant.direction == 1 || ant.direction == 5
-        xs = rot90(verticalBodyX);              %rot90 function is wrong, replace
-        ys = rot90(vertivalBodyY);
+        [xs, ys] = rot90n(verticalBodyX,verticalBodyY, -1);              
     elseif ant.direction == 3 || ant.direction == 7
         xs = verticalBodyX;
-        ys = vertivalBodyY;
+        ys = verticalBodyY;
     elseif ant.direction == 2 || ant.direction == 6
         xs = diagonalBodyX;
         ys = diagonalBodyY;
     else
-        xs = rot90(diagonalBodyX);
-        ys = rot90(diagonalBodyY);
+        [xs, ys] = rot90n(diagonalBodyX,diagonalBodyY, 1);
     end
     
     xs = reshape(xs, [1,numel(xs)]) + ant.pos(1);
     ys = reshape(ys, [1,numel(ys)]) + ant.pos(2);
 end
+
