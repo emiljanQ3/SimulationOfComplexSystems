e14b389d98a169ba08905580d4a1c7a9e3f5ca8d
Emil <emiljanQ3@hotmail.com>
2020-12-03 10:44:35 +0100

    Added DirectionalArea function

---
+++ MatlabAnthill/DirectionalArea.m
@@ -0,0 +1,17 @@
+function [xs,ys] = DirectionalArea(dirIndex)
+    rightX = [3,3,3];
+    rightY = [-1:1];
+    topRightX = [2,3,3];
+    topRightY = [3,3,2];
+    
+    if mod(dirIndex, 2) == 1
+        k = (dirIndex - 1)/2;
+        xs = rot90(rightX, k);       %rot90 function is wrong, replace
+        ys = rot90(rightY, k);
+    else
+        k = (dirIndex)/2 - 1;
+        xs = rot90(topRightX, k);
+        ys = rot90(topRightY, k);
+    end
+end
+
