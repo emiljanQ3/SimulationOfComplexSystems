2ef0f911b9fb763200e219e2944646f0afd99b39
Johan Nilsson <johan.laberg@outlook.com>
2020-12-04 13:31:01 +0100

    removed printing stuff

--- MatlabAnthill/VisualizeLayer.m
+++ MatlabAnthill/VisualizeLayer.m
@@ -1,15 +1,13 @@
 function VisualizeLayer(world, ants, showLayers)
-close all;
 %   Visualizes each layer of world according to the boolean array.
     names = fieldnames(world);
     figure(1); axis square; axis([0, length(world.sand), 0, length(world.sand)])
     tiledlayout(2,ceil(length(names)/2))
-    for i = 1:length(names)
+    for i = 1:length(names)-1
         nexttile
         if showLayers(i)
             A = getfield(world,names{i});
-            A(1:10,1:10)
-            image(255*A); hold on;
+            image(500*A); hold on;
             for j = 1:length(ants)
                 plot(ants(j).pos(1),ants(j).pos(2),'*r');
             end
