078e73be48235f42e46a0c299bdbfea29797aada
Emil <emiljanQ3@hotmail.com>
2020-12-04 09:25:10 +0100

    Fixed rot90n sometimes returning floating point values

--- MatlabAnthill/rot90n.m
+++ MatlabAnthill/rot90n.m
@@ -2,5 +2,8 @@ function [xRot, yRot] = rot90n(xMatrix, yMatrix, n)
     % rotate n times towards left if sign(n) > 0, else rotate right. 
     xRot = xMatrix * cos(n*pi/2) - yMatrix * sin(n*pi/2);
     yRot = xMatrix * sin(n*pi/2) + yMatrix * cos(n*pi/2);
+    
+    xRot = round(xRot);
+    yRot = round(yRot);
 end
 
