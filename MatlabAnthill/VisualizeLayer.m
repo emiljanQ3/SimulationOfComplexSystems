f750cdb0eafb3c6079d62c7f4141b1b6e90f0f70
Colton Cunov <coltoncunov@gmail.com>
2020-12-04 13:27:36 +0100

    add question comment

--- MatlabAnthill/VisualizeLayer.m
+++ MatlabAnthill/VisualizeLayer.m
@@ -1,6 +1,8 @@
 function VisualizeLayer(world, ants, showLayers)
 close all;
 %   Visualizes each layer of world according to the boolean array.
+% Q: what should showLayers input look like, and what does each element
+% correspond to?
     names = fieldnames(world);
     figure(1); axis square; axis([0, length(world.sand), 0, length(world.sand)])
     tiledlayout(2,ceil(length(names)/2))
