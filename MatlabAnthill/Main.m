cf80c83bd19c15d9c8f7dbac79b5a588c6224c44
Emil <emiljanQ3@hotmail.com>
2020-12-07 20:29:19 +0100

    Cleaned up visualization code

--- MatlabAnthill/Main.m
+++ MatlabAnthill/Main.m
@@ -44,32 +44,9 @@ for i = 1:numTimeSteps
 end
 
 %% Visualize
-sand = zeros(1, length(snapshots));
-pelletsTot = sand;
-pelletsInside = sand;
+VisualizeSandPelletsOverTime(snapshots)
 
 for i = 1:length(snapshots)
-    world = snapshots{i};
-    sand(i) = sum(world.sand, 'all');
-    pelletsTot(i) = sum(world.pellets, 'all');
-    pelletsInside(i) = sum(world.pellets(world.disk), 'all');
-end
-
-x = 1:length(snapshots);
-y1 = sand;
-y2 = sand + pelletsInside;
-y3 = sand + pelletsTot;
-
-figure(4)
-hold off
-plot(x, y1)
-hold on
-plot(x, y2)
-plot(x, y3)
-
-legend('Sand', 'Sand + pellets under disc', 'Sand + all pellets')
-
-for i = 4409:length(snapshots)
     world = snapshots{i};
     DrawWorld(world, 0);
 end
