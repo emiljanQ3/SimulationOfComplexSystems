8a46cea64837015858c077c33d3c5b35568198c7
Emil <emiljanQ3@hotmail.com>
2020-12-08 19:50:38 +0100

    Updated snapshots to save all te time

--- MatlabAnthill/SaveSnapshots.m
+++ MatlabAnthill/SaveSnapshots.m
@@ -1,6 +1,6 @@
 function snapshots = SaveSnapshots(snapshots, i, numTimeSteps, world)
     completion = i/numTimeSteps * length(snapshots);  %in percent
-    if mod(completion, 1) <= 1/(2*numTimeSteps)
+    if mod(completion, 1) <= 1/(numTimeSteps)
         index = round(completion);
         snapshots{index} = world;
     end
