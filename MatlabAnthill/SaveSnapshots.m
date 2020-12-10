41bdbfa6284af69511ed50b0bccc16eb9e6f7557
Emil <emiljanQ3@hotmail.com>
2020-12-10 10:39:14 +0100

    Fixed bug with empty snapshots

--- MatlabAnthill/SaveSnapshots.m
+++ MatlabAnthill/SaveSnapshots.m
@@ -1,6 +1,6 @@
 function snapshots = SaveSnapshots(snapshots, i, numTimeSteps, world)
     completion = i/numTimeSteps * length(snapshots);  %in percent
-    if mod(completion, 1) <= 1/(numTimeSteps)
+    if mod(completion, 1) <= 1/(numTimeSteps) || 1-mod(completion, 1) <= 1/(numTimeSteps)
         index = round(completion);
         snapshots{index} = world;
     end
